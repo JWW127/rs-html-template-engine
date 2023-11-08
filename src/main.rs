74a8c6b73a105f080c599e388d0b63121c2669d4
Joey <joseph.w.woolever@gmail.com>
2023-11-08 02:45:26 -0600

    initial

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+/*
+starting point of program. First calls fns to tokenize and parse template
+string, context data is fed into the template, then calls fns to generate
+HTML via parser output and context data
+ */
+fn main() {
+    println!("hello")
+}
